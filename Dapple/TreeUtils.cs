3f39e0789cb61234976773576a15ddc8aba68cf6
chris <chris@8bc002af-aa1e-4b94-b094-18545e236210>
2009-01-12 19:19:16 +0000

    -Commented out dead code (will delete later)

--- Dapple/TreeUtils.cs
+++ Dapple/TreeUtils.cs
@@ -1,144 +1,144 @@
-using System;
-using System.Windows.Forms;
-using System.Collections.Generic;
-using System.Text;
+//using System;
+//using System.Windows.Forms;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Dapple
-{
-   class TreeUtils
-   {
-      private TreeUtils()
-      {
-      }
+//namespace Dapple
+//{
+//   class TreeUtils
+//   {
+//      private TreeUtils()
+//      {
+//      }
       
-      /// <summary>
-      /// Determines whether parent/child relationship exist
-      /// </summary>
-      /// <param name="parentNode"></param>
-      /// <param name="childNode"></param>
-      /// <returns></returns>
-      public static bool isParent(TreeNode parentNode, TreeNode childNode)
-      {
-         if (parentNode == childNode)
-            return true;
+//      /// <summary>
+//      /// Determines whether parent/child relationship exist
+//      /// </summary>
+//      /// <param name="parentNode"></param>
+//      /// <param name="childNode"></param>
+//      /// <returns></returns>
+//      public static bool isParent(TreeNode parentNode, TreeNode childNode)
+//      {
+//         if (parentNode == childNode)
+//            return true;
 
-         TreeNode n = childNode;
-         bool bFound = false;
-         while (!bFound && n != null)
-         {
-            n = n.Parent;
-            bFound = (n == parentNode);
-         }
-         return bFound;
-      }
+//         TreeNode n = childNode;
+//         bool bFound = false;
+//         while (!bFound && n != null)
+//         {
+//            n = n.Parent;
+//            bFound = (n == parentNode);
+//         }
+//         return bFound;
+//      }
 
-      /// <summary>
-      /// Breadth first search for first treenode tag of type
-      /// </summary>
-      /// <param name="type"></param>
-      /// <param name="collection"></param>
-      /// <returns></returns>
-      public static TreeNode FindNodeOfTypeBFS(Type type, TreeNodeCollection collection)
-      {
-         System.Collections.Generic.Queue<TreeNode> queue = new Queue<TreeNode>();
-         foreach (TreeNode treeNode in collection)
-         {
-            queue.Enqueue(treeNode);
-         }
+//      /// <summary>
+//      /// Breadth first search for first treenode tag of type
+//      /// </summary>
+//      /// <param name="type"></param>
+//      /// <param name="collection"></param>
+//      /// <returns></returns>
+//      public static TreeNode FindNodeOfTypeBFS(Type type, TreeNodeCollection collection)
+//      {
+//         System.Collections.Generic.Queue<TreeNode> queue = new Queue<TreeNode>();
+//         foreach (TreeNode treeNode in collection)
+//         {
+//            queue.Enqueue(treeNode);
+//         }
 
-         while (queue.Count > 0)
-         {
-            TreeNode treeNode = queue.Dequeue();
-            if (treeNode.Tag != null && treeNode.Tag.GetType() == type)
-               return treeNode;
-            else
-            {
-               foreach (TreeNode tNode in treeNode.Nodes)
-               {
-                  queue.Enqueue(tNode);
-               }
-            }
-         }
+//         while (queue.Count > 0)
+//         {
+//            TreeNode treeNode = queue.Dequeue();
+//            if (treeNode.Tag != null && treeNode.Tag.GetType() == type)
+//               return treeNode;
+//            else
+//            {
+//               foreach (TreeNode tNode in treeNode.Nodes)
+//               {
+//                  queue.Enqueue(tNode);
+//               }
+//            }
+//         }
 
-         return null;
-      }
+//         return null;
+//      }
 
-      /// <summary>
-      /// Breadth first search for treenode with the same tag reference
-      /// </summary>
-      /// <param name="tag"></param>
-      /// <param name="collection"></param>
-      /// <returns></returns>
-      public static TreeNode FindNodeBFS(object tag, TreeNodeCollection collection)
-      {
-         System.Collections.Generic.Queue<TreeNode> queue = new Queue<TreeNode>();
-         foreach (TreeNode treeNode in collection)
-         {
-            queue.Enqueue(treeNode);
-         }
+//      /// <summary>
+//      /// Breadth first search for treenode with the same tag reference
+//      /// </summary>
+//      /// <param name="tag"></param>
+//      /// <param name="collection"></param>
+//      /// <returns></returns>
+//      public static TreeNode FindNodeBFS(object tag, TreeNodeCollection collection)
+//      {
+//         System.Collections.Generic.Queue<TreeNode> queue = new Queue<TreeNode>();
+//         foreach (TreeNode treeNode in collection)
+//         {
+//            queue.Enqueue(treeNode);
+//         }
 
-         while (queue.Count > 0)
-         {
-            TreeNode treeNode = queue.Dequeue();
-            if (treeNode.Tag == tag)
-               return treeNode;
-            else
-            {
-               foreach (TreeNode tNode in treeNode.Nodes)
-               {
-                  queue.Enqueue(tNode);
-               }
-            }
-         }
+//         while (queue.Count > 0)
+//         {
+//            TreeNode treeNode = queue.Dequeue();
+//            if (treeNode.Tag == tag)
+//               return treeNode;
+//            else
+//            {
+//               foreach (TreeNode tNode in treeNode.Nodes)
+//               {
+//                  queue.Enqueue(tNode);
+//               }
+//            }
+//         }
 
-         return null;
-      }
+//         return null;
+//      }
 
-      /// <summary>
-      /// Depth First search for a node by its tag
-      /// </summary>
-      /// <param name="col"></param>
-      /// <param name="tag"></param>
-      /// <returns></returns>
-      public static TreeNode FindNodeDFS(TreeNodeCollection col, object tag)
-      {
-         foreach (TreeNode treeNode in col)
-         {
-            if (treeNode.Tag == tag)
-            {
-               return treeNode;
-            }
-            else
-            {
-               TreeNode temp = FindNodeDFS(treeNode.Nodes, tag);
-               if (temp != null) return temp;
-            }
-         }
-         return null;
-      }
+//      /// <summary>
+//      /// Depth First search for a node by its tag
+//      /// </summary>
+//      /// <param name="col"></param>
+//      /// <param name="tag"></param>
+//      /// <returns></returns>
+//      public static TreeNode FindNodeDFS(TreeNodeCollection col, object tag)
+//      {
+//         foreach (TreeNode treeNode in col)
+//         {
+//            if (treeNode.Tag == tag)
+//            {
+//               return treeNode;
+//            }
+//            else
+//            {
+//               TreeNode temp = FindNodeDFS(treeNode.Nodes, tag);
+//               if (temp != null) return temp;
+//            }
+//         }
+//         return null;
+//      }
 
-      /// <summary>
-      /// Depth First search for a node by its tag (or string if tag is null)
-      /// </summary>
-      /// <param name="col"></param>
-      /// <param name="strtext"></param>
-      /// <param name="tag"></param>
-      /// <returns></returns>
-      public static TreeNode FindNodeDFS(TreeNodeCollection col, string strText, object tag)
-      {
-         foreach (TreeNode treeNode in col)
-         {
-            if ((tag == null && treeNode.Text == strText) || (tag != null && treeNode.Tag == tag))
-            {
-               return treeNode;
-            }
-            else
-            {
-               TreeNode temp = FindNodeDFS(treeNode.Nodes, strText, tag);
-               if (temp != null) return temp;
-            }
-         }
-         return null;
-      }
-   }
-}
+//      /// <summary>
+//      /// Depth First search for a node by its tag (or string if tag is null)
+//      /// </summary>
+//      /// <param name="col"></param>
+//      /// <param name="strtext"></param>
+//      /// <param name="tag"></param>
+//      /// <returns></returns>
+//      public static TreeNode FindNodeDFS(TreeNodeCollection col, string strText, object tag)
+//      {
+//         foreach (TreeNode treeNode in col)
+//         {
+//            if ((tag == null && treeNode.Text == strText) || (tag != null && treeNode.Tag == tag))
+//            {
+//               return treeNode;
+//            }
+//            else
+//            {
+//               TreeNode temp = FindNodeDFS(treeNode.Nodes, strText, tag);
+//               if (temp != null) return temp;
+//            }
+//         }
+//         return null;
+//      }
+//   }
+//}
